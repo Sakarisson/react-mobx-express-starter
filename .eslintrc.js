e7b52a866eb84a9c71d18d1d4afc61707691af2e
Kristian Sakarisson <kristian.sakarisson@druid.fi>
2018-08-25 11:32:57 +0300

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  "extends": ["airbnb"],
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "rules": {
+    "jsx-a11y/anchor-is-valid": [
+      "error", {
+        "components": [ "Link" ],
+        "specialLink": [ "to", "hrefLeft", "hrefRight" ],
+        "aspects": [ "noHref", "invalidHref", "preferButton" ]
+      }],
+    "react/jsx-filename-extension": [
+      1, {
+        "extensions": [
+          ".js",
+          ".jsx"
+        ]
+      }
+    ]
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "paths": [
+          path.resolve("src/client")
+        ]
+      }
+    }
+  }
+}
